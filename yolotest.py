5eaee646d55f305c677328592c5b93e3ab6f23ef
inheriat <wz119@koreatech.ac.kr>
2022-05-22 20:16:40 +0900

    yolo 테스트용 코드

---
+++ yolotest.py
@@ -0,0 +1,61 @@
+import torch
+import cv2
+import urllib
+import numpy as np
+# Model
+# class
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5n - yolov5x6, custom
+ 
+# Images
+#while(1):
+#img = 'https://ultralytics.com/images/zidane.jpg'  # or file, Path, PIL, OpenCV, numpy, list
+# 여기서부터 실시간으로 받는거임
+# zmq로 numpy를 받으면
+# 이게 돌아가기 시작하는거임
+#이거어케함?
+img = cv2.imread('yoloimage.jpg', 0)
+
+# Inference
+results = model(img)
+
+# Results
+results.print()
+df = results.pandas()  # or .show(), .save(), .crop(), .pandas(), etc.
+df1 = df.xyxy[0]
+print(df1)
+arealist = []
+ava = []
+bva = []
+
+dfhuman = df1[df1['class'] == 0]
+
+if len(dfhuman.index) != 0:
+    length = len(dfhuman.index)
+    dfhuman_xmax = dfhuman['xmax'].values
+    dfhuman_xmix = dfhuman['xmin'].values
+    dfhuman_ymax = dfhuman['ymax'].values
+    dfhuman_ymin = dfhuman['ymin'].values
+    for i in range(length):
+
+        xval = dfhuman_xmax[i] - dfhuman_xmix[i]
+        yval = dfhuman_ymax[i] - dfhuman_ymin[i]
+        area = xval * yval
+        ava.append(xval)
+        bva.append(yval)
+        arealist.append(int(area))
+
+
+print(dfhuman)
+
+print(arealist)
+#넓이 목록에서
+#넓이 몇 이상인게 있음?
+# True
+# True / False를 다시 ZMQ로 전송해야댐
+# 보내는건 할수있지
+#근데 받고실행하는거
+# 2번째로 걱정해야할 사항
+# 얼마나걸림?
+# 그걸 예상하고 가정할 사이즈를 줄이기?
+print(ava)
+print(bva)
